7f2667bba094d4097c1708fe530d030d89378211
erico <erico_pt@hotmail.com>
2023-05-30 15:18:59 -0300

    done create element & display functions

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 
 function requestInfo(location){
-    fetch(`http://api.weatherapi.com/v1/current.json?key=6915f6a4a5c5410c813184808232605&q=${location}`, {mode: 'cors'})
+    return fetch(`http://api.weatherapi.com/v1/current.json?key=6915f6a4a5c5410c813184808232605&q=${location}`, {mode: 'cors'})
     .then(res => res.json())
     .then(data => filterData(data))
+    .catch(err => alert(err))
     
 
 }
 
-requestInfo('london')
 
 
-const filterData = async (data) => {
-    const filtered = await {
+
+const filterData = (data) => {
+    const filtered =  {
         name: data.location.name,
         tempCelsius: data.current.temp_c,
         tempFarenheit: data.current.temp_f,
         humidity: data.current.humidity
     }
     console.log(filtered)
+    
     return filtered
     
 }
 
+const city = document.getElementById('city')
+const infoDiv = document.querySelector('.refresh')
+
+const submitBtn = document.querySelector('button')
+submitBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
+    infoDiv.innerHTML = ''
+    display()
+    
+})
+
+
+const createContainer = async (city) => {
+    const div = document.createElement('div')
+    const para = document.createElement('p')
+    const span = document.createElement('span')
+    const h2 = document.createElement('h2')
+    const myData = await requestInfo(city)
+    
+
+    para.textContent = `The temperature on ${myData.name} right now is:`
+    span.textContent = myData.tempCelsius + ' C'
+    h2.textContent = 'Humidity: ' + myData.humidity + '%'
+
+    div.classList = 'container'
+    div.appendChild(para)
+    div.appendChild(span)
+    div.appendChild(h2)
+
+    infoDiv.appendChild(div)
+}
+
+const display = () => {
+    createContainer(city.value)
+}
